b7acfa07eb70387d06038b251ed6ddfbb1173a10
Consciousnesszz <consciousnesszz1@gmail.com>
2018-07-26 10:27:21 +0800

    add linked list

---
+++ src/linkedList.js
@@ -0,0 +1,15 @@
+/**
+ * 链表结构
+ */
+class LinkedListElement {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+export class LinkedList {
+  constructor() {
+
+  }
+}
