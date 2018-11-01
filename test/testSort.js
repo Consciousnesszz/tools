0955673ff2103027ca2b46c0cc41ed21419143ff
zhangjh <zhangjh@chinawyny.com>
2018-11-01 11:19:06 +0800

    撤销更改，单元测试中勿包含逻辑

--- test/testSort.js
+++ test/testSort.js
@@ -2,12 +2,6 @@
 import { expect } from 'chai'
 import * as sortFunc from '../src/sortFunc'
 
-const positiveSequence = [1, 2, 3, 5, 6, 8, 9, 12, 25]
-const invertedSequence = [99, 15, 9, 8, 7, 6, 5, 4, 3]
-const outInOrder = [6, 8, 7, 9, 17, 30, 4, 3, 2]
-const repeatingSequence = [6, 8, 7, 88, 77, 77, 9, 9, 3, 2]
-const standardSortFunc = (arr) => arr.concat([]).sort((a, b) => a - b)
-
 describe('测试排序方法', function () {
   for (let prop in sortFunc) {
     describe(`测试 ${prop}`, function () {
@@ -18,16 +12,16 @@ describe('测试排序方法', function () {
         expect(sortFunc[prop]([1])).to.be.deep.equal([1])
       })
       it('正序数组应该不做更改', function () {
-        expect(sortFunc[prop](positiveSequence)).to.be.deep.equal(standardSortFunc(positiveSequence))
+        expect(sortFunc[prop]([1, 2, 3, 5, 6, 8, 9, 12, 25])).to.be.deep.equal([1, 2, 3, 5, 6, 8, 9, 12, 25])
       })
       it('倒序数组应该排列成为正序', function () {
-        expect(sortFunc[prop](invertedSequence)).to.be.deep.equal(standardSortFunc(invertedSequence))
+        expect(sortFunc[prop]([99, 15, 9, 8, 7, 6, 5, 4, 3])).to.be.deep.equal([3, 4, 5, 6, 7, 8, 9, 15, 99])
       })
       it('乱序数组应该排列成为正序', function () {
-        expect(sortFunc[prop](outInOrder)).to.be.deep.equal(standardSortFunc(outInOrder))
+        expect(sortFunc[prop]([6, 8, 7, 9, 17, 30, 4, 3, 2])).to.be.deep.equal([2, 3, 4, 6, 7, 8, 9, 17, 30])
       })
       it('重复元素数组应该排序正常', function () {
-        expect(sortFunc[prop](repeatingSequence)).to.be.deep.equal(standardSortFunc(repeatingSequence))
+        expect(sortFunc[prop]([6, 8, 7, 88, 77, 77, 9, 9, 3, 2])).to.be.deep.equal([2, 3, 6, 7, 8, 9, 9, 77, 77, 88])
       })
     })
   }
